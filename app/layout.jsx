155e0dbecc9c621ccd34d5fad1de5392b651ba82
vyasmohle <vyasmohle1811@gmail.com>
2026-01-23 14:42:15 +0530

    search logo changed

--- app/layout.jsx
+++ app/layout.jsx
@@ -45,8 +45,14 @@ export default function RootLayout({ children }) {
         <link
           rel="icon"
           type="image/png"
-          sizes="96x96"
-          href="/favicon-96x96.png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
         />
 
         {/* SVG fallback */}
